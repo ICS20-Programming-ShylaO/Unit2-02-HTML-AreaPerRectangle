335ac1650758088c479ea17bbba062f478101f8d
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-08 16:13:39 +0000

    updated index, script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Shyla Oommen All rights reserved
+//
+// Created by: Shyla Oommen
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function calculates perimeter and area
+  document.getElementById("area").innerHTML = "The area is " + (6 * 8) + "cm<sup>2</sup>"
+  document.getElementById("perimeter").innerHTML = "The perimeter is " + 2 * (6 + 8) + "cm"
+}
